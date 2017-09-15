a6fbcb07db3ec95c993425069c3ad801594db4a8
Yen-Kuan Wu <c14006078@gmail.com>
2017-09-15 17:03:05 +0800

    tmp commit for reinstalling OS

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -1,12 +1,16 @@
 class Roulette():
-    def __init__(self, policy, cands = ()):
+    def __init__(self, policy, cands = (None)):
+        self.__policy = None
+        self.__candidates = None
         self.setUp(policy, cands)
 
-    def setUp(self, plcy = None, cands = None):
-        self.__policy = plcy if plcy != None else self.policy
-        self.__candidates = cands if cands != None else self.candidates
+    @property
+    def setUp(self, plcy = None, cands = (None)):
+        self.__policy = plcy if plcy != None else self.__policy
+        self.__candidates = cands if cands != None else self.__candidates
         self.__ptr = None
 
+    @property
     def lottery(self):
         self.__ptr = self.__policy(self.__ptr, self.__candidates)
         return self.__ptr
